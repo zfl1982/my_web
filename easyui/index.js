a61f229dfc58febcfca87bd0646119dd3e36d7c4
zfl <zfl@zfll-PC>
2017-06-08 15:58:18 +0800

    添加表单

---
+++ easyui/index.js
@@ -0,0 +1,32 @@
+		function addTab(title, href,iconCls,iframe){
+			var tabPanel = $('#admin-tabs');
+			var href=href;
+			if(!tabPanel.tabs('exists',title)){
+				var content = '<iframe scrolling="auto" frameborder="0"  src="'+ href +'" style="width:100%;height:100%;"></iframe>';
+				if(iframe){
+					tabPanel.tabs('add',{
+						title:title,
+						content:content,
+						//href:href,
+						iconCls:iconCls,
+						fit:true,
+						cls:'pd3',
+						closable:true
+					});
+				}
+				else{
+					tabPanel.tabs('add',{
+						title:title,
+						href:href,
+						iconCls:iconCls,
+						fit:true,
+						cls:'pd3',
+						closable:true
+					});
+				}
+			}
+			else
+			{
+				tabPanel.tabs('select',title);
+			}
+		}
